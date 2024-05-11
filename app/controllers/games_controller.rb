f531fb592d56e0e01f224283f90a63537c452f3a
David Wenk <davidwenk@icloud.com>
2024-05-11 12:03:50 +0200

    add word input html

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,9 @@
 class GamesController < ApplicationController
   def new
-    @letters = (0...10).map { ('a'..'z').to_a[rand(26)].upcase }
+    @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }
   end
 
   def score
+    raise
   end
 end
