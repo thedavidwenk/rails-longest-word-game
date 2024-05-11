bb71d0bdbfe67a7eb9ac72a0bec12ca18c1ad5f7
David Wenk <davidwenk@icloud.com>
2024-05-11 20:36:11 +0200

    add feature to count points

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,45 +5,33 @@ class GamesController < ApplicationController
   def new
     @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }
     session[:letters] = @letters
+    session[:points] ||= 0 
   end
 
   def score
     @answer = params[:answer]
     @letters = session[:letters] || []
-    
+
     begin
-      url = "https://dictionary.lewagon.com/#{@answer}"
-      word_serialized = URI.open(url).read
-      word = JSON.parse(word_serialized)
+      word = JSON.parse(URI.open("https://dictionary.lewagon.com/#{@answer}").read)
+    rescue => error
+      # Handle API errors here (more on this later)
+      return "Error checking word: #{error.message}"
     end
-
-
 
     def same_letters?(letters, answer)  
       answer.chars.all? { |char| letters.include?(char) } 
     end
 
     if  word["found"] && same_letters?(@letters, @answer.upcase)
-      @response = "The word is valid according to the grid and is an English word ✅"
+      @points = word["length"] 
+      session[:points] += @points
+      @response = "The word is valid according to the grid and is an English word. You earned #{@points} points! 🎉 "
     elsif !word["found"] && same_letters?(@letters, @answer.upcase)
       @response = "The word is valid according to the grid, but is not a valid English word ❌"
     else !same_letters?(@letters, @answer)
       @response = "The word can’t be built out of the original grid ❌"
     end
 
-
-    # show the score on /score page
-    # pass params (done)
-    # display the letters array (done)
-    # parse the API (done)
-    #
-    # word
-    # => {"found"=>true, "word"=>"test", "length"=>4}
-
-    # to check if word is english
-    # define if / else logic 
-    # display a play again button (done)
-
-   #  raise
   end
 end
